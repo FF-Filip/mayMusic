ddcb2d6a275c4d41bae6f47bd56973618242d7ff
FF-Filip <filipflak.2005@wp.pl>
2022-12-24 12:34:38 +0100

    setting up router

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Backend from './Sites/Backend-demo';
-import App from './Sites/App';
+import App from './Components/App';
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={router} />
 );
